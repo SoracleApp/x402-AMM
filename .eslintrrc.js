7520464f7187bba9bfcf62972a469a77f8aa8afe
pkxro <cognitiveorganism@gmail.com>
2025-10-29 02:10:30 +0100

    Create .eslintrrc.js

---
+++ .eslintrrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json", "./packages/**/tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "promise"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  env: {
+    node: true,
+  },
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-floating-promises": "error",
+    "promise/catch-or-return": "warn",
+    "@typescript-eslint/no-misused-promises": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+  ignorePatterns: [".eslintrc.js"],
+};

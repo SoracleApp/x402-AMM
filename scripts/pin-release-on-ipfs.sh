bbb1d1a546accd36ea78fcf1a156224ff035f392
pkxro <cognitiveorganism@gmail.com>
2025-10-29 02:14:20 +0100

    Create pin-release-on-ipfs.sh

---
+++ scripts/pin-release-on-ipfs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Found: https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+CURRENT_VERSION=$(curl --silent "https://api.github.com/repos/AugurProject/turbo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/');
+VERSION="${VERSION:-$CURRENT_VERSION}"
+APP="${APP:-turbo}"
+curl -OL "https://github.com/AugurProject/turbo/releases/download/$VERSION/$APP.tar.gz";
+
+tar -xzf $APP.tar.gz;
+
+IPFS_HASH_CIDv0=$(ipfs add -rq --silent build | sed '$!d');
+IPFS_HASH_CIDv1=$(ipfs cid base32 $IPFS_HASH_CIDv0);
+
+echo "CIDv0: $IPFS_HASH_CIDv0";
+echo "CIDv1: $IPFS_HASH_CIDv1";
+
+rm -rf build;
+rm $APP.tar.gz;
+
+echo "Propagating Hash";
+curl --retry 100 "https://$IPFS_HASH_CIDv1.ipfs.dweb.link" ;
